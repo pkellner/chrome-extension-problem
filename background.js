313e977b2d936c6bb8eb392aaf9551b8356dfc4f
pkellner <peter@peterkellner.net>
2019-05-18 11:17:29 -0700

    messaging working

--- background.js
+++ background.js
@@ -1,4 +1,68 @@
-let nasaPageShowing = false;
+// chrome.browserAction.onClicked.addListener(() => {
+//   chrome.tabs.executeScript({
+//     file: "toggleNasaImage.js"
+//   });
+// });
+
+// var urlxxx =
+//   "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/p5020056.jpg";
+var url =
+  "https://ddrt7tzfkdwdf.cloudfront.net/Images/silicon-valley-code-camp.png";
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.tabs.executeScript({ file: "toggleNasaImage.js" }, function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { url: url }, function(response) {
+        console.log("abcdefg");
+      });
+    });
+  });
+});
+
+// chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//   chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function(
+//     response
+//   ) {
+//     console.log('abcd');
+//   });
+// });
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//   chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});
+// });
+
+// chrome.tabs.executeScript(
+//   { file: "/content_scripts/beastify.js" },
+//   results => {
+//     console.log(
+//       `chrome.tabs.executeScript:nasaPageShowing:${nasaPageShowing}`
+//     );
+//     if (nasaPageShowing === false && firstTime === true) {
+//       chrome.tabs.query({ active: true, currentWindow: true }, beastify);
+//       nasaPageShowing = true;
+//       firstTime = false;
+//     } else if (nasaPageShowing === false && firstTime === false) {
+//       chrome.tabs.query({ active: true, currentWindow: true }, beastify);
+//       nasaPageShowing = true;
+//     } else {
+//       nasaPageShowing = false;
+//       chrome.tabs.query({ active: true, currentWindow: true }, reset);
+//     }
+//   }
+// );
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+//chrome.tabs.executeScript({ file: "/content_scripts/beastify.js" }, results => {
+//chrome.tabs.insertCSS({ code: hidePage }, () => {
+//let url = "https://apod.nasa.gov/apod/image/1904/LagoonPanSTARRS_ColesPugh1024.jpg";
+// might need to make sure we have class beautify
+//chrome.tabs.query({ active: true, currentWindow: true }, beastify);
+//});
+//});
 
 /**
  * CSS to hide everything on the page,
@@ -36,47 +100,14 @@ let nasaPageShowing = false;
 //   });
 // }
 
-chrome.browserAction.onClicked.addListener(() => {
-  console.log("x");
+// https://stackoverflow.com/questions/4976996/tabs-executescript-passing-parameters-and-using-libraries
+//   https://stackoverflow.com/questions/17567624/pass-a-parameter-to-a-content-script-injected-using-chrome-tabs-executescript
 
-  chrome.tabs.executeScript({
-    file: "toggleNasaImage.js"
-  });
+// chrome.tabs.executeScript(tab.id, {file: 'content.js'}, function() {
+//   chrome.tabs.sendMessage(tab.id, 'whatever value; String, object, whatever');
+// });
 
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-  //   chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});
-  // });
-
-  // chrome.tabs.executeScript(
-  //   { file: "/content_scripts/beastify.js" },
-  //   results => {
-  //     console.log(
-  //       `chrome.tabs.executeScript:nasaPageShowing:${nasaPageShowing}`
-  //     );
-  //     if (nasaPageShowing === false && firstTime === true) {
-  //       chrome.tabs.query({ active: true, currentWindow: true }, beastify);
-  //       nasaPageShowing = true;
-  //       firstTime = false;
-  //     } else if (nasaPageShowing === false && firstTime === false) {
-  //       chrome.tabs.query({ active: true, currentWindow: true }, beastify);
-  //       nasaPageShowing = true;
-  //     } else {
-  //       nasaPageShowing = false;
-  //       chrome.tabs.query({ active: true, currentWindow: true }, reset);
-  //     }
-  //   }
-  // );
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  // Handle message.
+  // In this example, message === 'whatever value; String, object, whatever'
 });
-
-/**
- * When the popup loads, inject a content script into the active tab,
- * and add a click handler.
- * If we couldn't inject the script, handle the error.
- */
-//chrome.tabs.executeScript({ file: "/content_scripts/beastify.js" }, results => {
-//chrome.tabs.insertCSS({ code: hidePage }, () => {
-//let url = "https://apod.nasa.gov/apod/image/1904/LagoonPanSTARRS_ColesPugh1024.jpg";
-// might need to make sure we have class beautify
-//chrome.tabs.query({ active: true, currentWindow: true }, beastify);
-//});
-//});
